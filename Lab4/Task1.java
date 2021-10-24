1e5a4f2288a3b21b756b791cac77acbc8345f7da
senpainikolay <39433010+senpainikolay@users.noreply.github.com>
2021-10-25 01:16:33 +0300

    Add files via upload

---
+++ Lab4/Task1.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        try {
+            //
+            // PATH
+                File pathFile = new File("one_expression.txt");
+            //
+            //
+            Scanner scannerObj = new Scanner(pathFile);
+
+            while ( scannerObj.hasNextLine() ) {
+                String data = scannerObj.nextLine(); //  data =  stores per 1 line of .txt file
+                System.out.println(data);
+                checkParanthesis(data);
+            }
+            scannerObj.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void checkParanthesis(String data ) {
+        Stack paranStack = new Stack();
+        int stringLen = data.length();
+        int indexChar = 0;
+        while (indexChar != stringLen  ) {
+
+            if ((data.charAt(indexChar) == ')') && (paranStack.isEmpty() == true))
+            {
+                System.out.println("Parenthesis are NOT put  correct! ");
+                return;
+            }
+
+            if (data.charAt(indexChar) == '(') paranStack.push(data.charAt(indexChar));
+            if (data.charAt(indexChar) == ')') paranStack.pop();
+
+            indexChar++;
+
+        }
+
+        if ( paranStack.isEmpty() == true )  System.out.println("Parenthesis are put correct.");
+        else System.out.println("Parenthesis are NOT put  correct!");
+
+    }
+
+
+}
